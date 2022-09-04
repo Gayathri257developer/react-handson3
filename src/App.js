7a4caccabf6c67521c6eb5c99b5c8474c21a0fb6
gayathri <gayathri.ramakrishna158@gmail.com>
2022-09-04 15:01:34 +0530

    handson3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
 import FeedbackData from './Components/FeedbackData';
-import FeedbackForm from './Components/FeedbackForm'
 
  class App extends Component {
   render() {
 
     return (
       <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/feedbackdata' element={<FeedbackData/>}/>
-        <Route path="/" element={<FeedbackForm/>} />
-      </Routes>
-      </BrowserRouter>
       
+      <FeedbackData/>
       </div>
     )
   }
 }
+// <BrowserRouter>
+//       <Routes>
+//         <Route path='/feedbackdata' element={<FeedbackData/>}/>
+//         <Route path="/" element={<FeedbackForm/>} />
+//       </Routes>
+//       </BrowserRouter>
 export default App
--- src/Components/FeedbackData.js
+++ src/Components/FeedbackData.js
@@ -1,24 +1,61 @@
-import React, { Component } from 'react'
-import './Data.css';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import './Data.css'
 
- class FeedbackData extends Component {
-  render() {
-    return (
-      <>
-         <h2>Employee feedback data</h2>
-         <br/>
-         <div className='outerbox'>
-           <div className='databox'>
-              
-           </div>
-         </div>
-         <Link to="/">
-         <button>Go Back</button>
-         </Link>
-         
-      </>
-    )
+const FeedbackData = () => {
+
+  const [input, setData] = useState({name:"",department:"",rating:"",})
+  const[arrofobj,setArrofobj] =useState([])
+  const[hidden,setHidden] =useState(0)
+
+  const [counter,setCounter] = useState(0);
+  const inputChange = (e) => {
+        setData({...input,[e.target.name]:e.target.value});
   }
+
+  const onsubmits = (e) =>{
+    e.preventDefault();
+    setCounter(counter+1)
+    const arr={...input,id:counter}
+    setArrofobj([...arrofobj,arr])
+    setData({name:"",department:"",rating:"",})
+    
+    setHidden(!hidden)
+  }
+
+  const goBack =()=>{
+    setHidden(!hidden)
+  }
+  
+
+  return (
+    <>
+      <div style={{'display':hidden===true ? 'none':'unset'}} >
+          <h2>employee feedback form</h2>
+          <form onSubmit={onsubmits}>
+            <label >Name :
+            <input type="text" name="name" id="name" value={input.name}  onChange={inputChange} />
+            </label><br/>
+            <label >Department :
+            <input type="text" name="department" id="department" value={input.department} onChange={inputChange} />
+            </label><br/>
+            <label >Rating :
+            <input type="number" name="rating" id="rating" value={input.rating} onChange={inputChange} />
+            </label><br/>
+            <input type="submit"/>
+          </form>
+          
+      </div>
+      <div style={{'display':hidden===true ? 'unset':'none'}} >
+        <h2>employee feedback data</h2>
+          <div className='outerbox'>
+            {arrofobj.map((item)=>{
+                return <div className='databox'>Name : {item.name } | Department : {item.department} | Rating : {item.rating}</div>
+            })}
+          </div>
+          <button onClick={goBack}>Go Back</button>
+      </div>
+     
+    </>
+  );
 }
-export default FeedbackData
+export default FeedbackData;
