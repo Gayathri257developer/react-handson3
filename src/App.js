ba0137e49792f8c08f5ce1a7411f782377111d91
Gayathri D R <99469991+gayathri257developer@users.noreply.github.com>
2022-09-08 12:07:06 +0530

    Update App.js
    recode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react'
-import FeedbackData from './Components/FeedbackData';
+import React from 'react'
 
- class App extends Component {
-  render() {
+import FeedbackData from "./Components/FeedbackData";
 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      depart: '',
+      rating:'',
+      users:[]
+    }
+  }
+
+  updateName = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  updateDepartment = (event) => {
+    this.setState({depart: event.target.value});
+  }
+
+  updateRating = (event) => {  
+    this.setState({rating: event.target.value});
+  }
+
+
+  handleOnClick  = () => {
+    const updatedUsers = this.state.users;
+
+   updatedUsers.push({name:this.state.name, depart:this.state.depart, rating:this.state.rating})
+    this.setState({users:updatedUsers, name:'', depart:'', rating:'' })
+  
+  }
+
+    render() {
     return (
-      <div>
+      <>
+      <div className="form">
+      <h1> Emoplyee Feedback Form</h1>
+      Name:<input value={this.name} type="text" onChange={this.updateName} />
+      <br />
+      Department:<input value={this.depart} type="text" onChange={this.updateDepartment}/>
+      <br/>
+      Rating:<input value={this.rating} type="number" onChange={this.updateRating}/>  
+      <button id='btn1' onClick={this.handleOnClick}> Submit </button>
+      </div>  
       
-      <FeedbackData/>
-      </div>
+
+    <FeedbackData name={this.state.name} depart={this.state.depart} rating={this.state.rating} users={this.state.users} 
+    updateName={this.updateName} updateDepartment={this.updateDepartment} updateRating={this.updateRating}/> 
+      </>
+    
     )
   }
 }
-// <BrowserRouter>
-//       <Routes>
-//         <Route path='/feedbackdata' element={<FeedbackData/>}/>
-//         <Route path="/" element={<FeedbackForm/>} />
-//       </Routes>
-//       </BrowserRouter>
-export default App
+
+export default App;
+
